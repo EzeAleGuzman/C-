c095284fb04a5f3b28203ea7a9fa03445849a4e4
Sarabe89 <ezealeguzman@gmail.com>
2024-01-01 12:22:21 -0300

    Agrego el git ignore

---
+++ curso C# nivel1/ejercicios2/TiempodeViaje/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TiempodeViaje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // Instancio las variables
+        int distancia, velocidad, resultado;
+
+        // Solicito la distancia por pantalla
+        Console.WriteLine("Distancia Entre Ciudades: ");
+        // Capturo el valor y lo asigno a mi variable
+        distancia = int.Parse(Console.ReadLine());
+        // Solicito La velocidad por pantalla
+        Console.WriteLine("velocidad Promedio: ");
+        // Capturo el valor y lo asigno a su variable
+        velocidad = int.Parse(Console.ReadLine());
+
+        // Realizo la operacion 
+
+        }
+    }
+}
