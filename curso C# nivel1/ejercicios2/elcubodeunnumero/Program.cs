73e51c34cc2db69692f5eaf0195d8c4cd441cc89
Sarabe89 <ezealeguzman@gmail.com>
2024-01-01 11:40:46 -0300

    unidad 1 y 2

---
+++ curso C# nivel1/ejercicios2/elcubodeunnumero/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace elvadoalcuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // Defino las Variables
+        int num, resultado;
+
+        // Pido Por pantalla que se ingrese un nuimero
+        Console.WriteLine("Ingese un numero: ");
+
+        // igualo mi variable al ingreso por pantalla
+        num = int.Parse(Console.ReadLine());
+
+        //hago la operacion y asigno el resultado
+        resultado = num * num * num;
+
+        // imprimo por pantalla el resltado concatenando
+        Console.WriteLine(" el cubo de " + num + " es " + resultado);
+
+
+
+
+
+
+        }
+    }
+}
---
+++ curso C# nivel1/ejercicios2/suma/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // paso 0
+            // Tipo de dato : Int, float, char, bool
+            int n1, n2, resultado;
+
+
+            // Pedir valores
+            Console.WriteLine("Ingrese un numero");
+            n1 =  int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero: ");
+            n2 = int.Parse(Console.ReadLine());
+            // Realizar calculo
+            resultado = n1 * n2;
+            // Emitir el resultado por pantalla
+            Console.WriteLine("el resultado es : " + resultado);
+        }
+    }
+}
