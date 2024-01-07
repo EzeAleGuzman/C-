400bd4c60922c3ac3622f779c3e422e96ea82998
Sarabe89 <ezealeguzman@gmail.com>
2024-01-07 13:53:57 -0300

    acomodando carpetas

---
+++ curso C# nivel1/Ejercicios3/mayorMenora10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace mayorMenora10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        // Hacer un programa para ingresar un número y luego se emita por pantalla un cartel aclaratorio si “Es mayor a 10” o “No es mayor a 10”.
+        
+        int num;
+
+        Console.WriteLine("Dame un numero: ");
+        num = int.Parse(Console.ReadLine());
+
+
+        if (num > 10) {
+            Console.WriteLine("el numero  es mayor a 10");
+        }
+        else if (num < 10){
+            Console.WriteLine("El numero es menor a 10");
+        }
+        else
+        {
+            Console.WriteLine("el numero es 10");
+        }
+       
+    }
+}
+}
