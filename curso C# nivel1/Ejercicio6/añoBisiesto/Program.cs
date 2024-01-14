61fbed202f111a10b1aef71bcb6d91c87b102d2d
Sarabe89 <ezealeguzman@gmail.com>
2024-01-13 22:55:22 -0300

    numeromayor de los agregados

--- curso C# nivel1/Ejercicio6/añoBisiesto/Program.cs
+++ curso C# nivel1/Ejercicio6/añoBisiesto/Program.cs
@@ -8,14 +8,16 @@ namespace añoBisiesto
         {
         //    Escribir un programa que permita al usuario ingresar dos años y luego imprima todos los años en ese rango, que sean bisiestos y múltiplos de 10. Nota: para que un año sea bisiesto debe ser divisible por 4 y no debe ser divisible por 100, excepto que también sea divisible por 400.
 
+        //instancio las variables
         int año1, año2;
         
+        //las solicito por pantalla ñlos años y los asigno a su variable
         Console.WriteLine("Dime el primer año: ");
         año1 = int.Parse(Console.ReadLine());
         Console.WriteLine("Dime el segundo año: ");
         año2 = int.Parse(Console.ReadLine());
 
-
+        // un loop
         for (int x= año1; x < año2;x++){
             if ((x % 4 ==0 && x % 100 != 0 )|| x % 400==0){
                 Console.WriteLine(x);
---
+++ curso C# nivel1/Ejercicio6/mayorIngreso/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace mayorIngreso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //instancio las variables
+            int num, mayor;
+            // igualo el mayor a 0 para luego 
+            mayor = 0;
+
+            Console.WriteLine("ingrese un numero: ");
+            num = int.Parse(Console.ReadLine());
+
+            while(num != 0){
+                if (num > mayor){
+                    mayor = num;
+                }
+                Console.WriteLine("ingrese un numero: ");
+            num = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("el numero ingresado  mayor es: "+ mayor);
+        }
+    }
+}
